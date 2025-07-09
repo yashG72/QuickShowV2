d0414dfb90f2998c9362d9ced2d4dd2ebac6470a
yashG72 <yashgoyal2874@gmail.com>
2025-07-09 13:07:47 +0530

    feat: implement email sending functionality with nodemailer and update package-lock.json

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
-import { sendEmail } from "../configs/nodeMailer.js";
+// import { sendEmail } from "../configs/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
@@ -90,26 +90,26 @@ const sendBookingConfirmationEmail = inngest.createFunction(
       })
       .populate("user");
 
-    await sendEmail({
-      to: booking.user.email,
-      subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
-      body: `<div style="font-family: Arial, sans-serif; line-height: 1.5;">
-        <h2>Hi ${booking.user.name},</h2>
-        <p>Your booking for <strong style="color: #F84565;">"${
-          booking.show.movie.title
-        }"</strong> is confirmed.</p>
-        <p>
-          <strong>Date:</strong> ${new Date(
-            booking.show.showDateTime
-          ).toLocaleDateString("en-US", { timeZone: "Africa/Kigali" })}<br />
-          <strong>Time:</strong> ${new Date(
-            booking.show.showDateTime
-          ).toLocaleTimeString("en-US", { timeZone: "Africa/Kigali" })}
-        </p>
-        <p>Enjoy the show! 🍿</p>
-        <p>Thanks for booking with us!<br />- QuickShow Team</P>
-      </div>`,
-    });
+    // await sendEmail({
+    //   to: booking.user.email,
+    //   subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
+    //   body: `<div style="font-family: Arial, sans-serif; line-height: 1.5;">
+    //     <h2>Hi ${booking.user.name},</h2>
+    //     <p>Your booking for <strong style="color: #F84565;">"${
+    //       booking.show.movie.title
+    //     }"</strong> is confirmed.</p>
+    //     <p>
+    //       <strong>Date:</strong> ${new Date(
+    //         booking.show.showDateTime
+    //       ).toLocaleDateString("en-US", { timeZone: "Africa/Kigali" })}<br />
+    //       <strong>Time:</strong> ${new Date(
+    //         booking.show.showDateTime
+    //       ).toLocaleTimeString("en-US", { timeZone: "Africa/Kigali" })}
+    //     </p>
+    //     <p>Enjoy the show! 🍿</p>
+    //     <p>Thanks for booking with us!<br />- QuickShow Team</P>
+    //   </div>`,
+    // });
   }
 );
 
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import showRouter from "./routes/showRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
+// import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
 
 const app = express();
 const port = 3000;
@@ -17,11 +17,11 @@ const port = 3000;
 await connectDB();
 
 // Stripe Webhooks Route
-app.use(
-  "/api/stripe",
-  express.raw({ type: "application/json" }),
-  stripeWebhooks
-);
+// app.use(
+//   "/api/stripe",
+//   express.raw({ type: "application/json" }),
+//   stripeWebhooks
+// );
 
 // Middleware
 app.use(express.json());
